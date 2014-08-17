ac762feede587771e0faf145cbd4b9fb06e50d2f
Petyo Cholakov <pecholakov@gmail.com>
2014-08-18 00:22:48 +0300

    Added more functionality to Account and more fields to accounts table (recomended proteins,carbs and fats)

--- src/account.py
+++ src/account.py
@@ -10,8 +10,14 @@ Base = declarative_base()
 class Account:
 
     @classmethod
-    def __init__(cls, session, name, password, weight, height, gender, activity_level, age):
+    def __init__(cls, session):
         cls.session = session
+
+# TODO: add check for proportions
+    @classmethod
+    def set_account(cls, name, password, weight, height, gender,
+                    activity_level, age, percent_proteins, percent_carbs,
+                    percent_fats):
         cls.name = name
         cls.password = password
         cls.weight = weight
@@ -19,17 +25,32 @@ class Account:
         cls.gender = gender
         cls.activity_level = activity_level
         cls.age = age
+        cls.percent_proteins = percent_proteins
+        cls.percent_carbs = percent_carbs
+        cls.percent_fats = percent_fats
+        cls.recomended_calories = cls.__calculate_recomended_calories__()
+        cls.recomended_proteins = \
+            cls.recomended_calories * cls.percent_proteins
+        cls.recomended_carbs = cls.recomended_calories * cls.percent_carbs
+        cls.recomended_fats = cls.recomended_calories * cls.percent_fats
 
-    @classmethod    
+    @classmethod
     def add_account(cls):
-        account = models.Account(name = cls.name,
-                                 password = cls.__crypt__(cls.password),
-                                 weight = cls.weight,
-                                 height = cls.height,
-                                 gender = cls.gender,
-                                 activity_level = cls.activity_level,
-                                 age = cls.age,
-                                 recomended_calories = cls.__calculate_recomended_calories__())
+        account = models.Account(
+            name=cls.name,
+            password=cls.__crypt__(cls.password),
+            weight=cls.weight,
+            height=cls.height,
+            gender=cls.gender,
+            activity_level=cls.activity_level,
+            age=cls.age,
+            recomended_calories=cls.recomended_calories,
+            recomended_proteins=cls.recomended_proteins,
+            recomended_carbs=cls.recomended_carbs,
+            recomended_fats=cls.recomended_fats,
+            percent_proteins=cls.percent_proteins,
+            percent_carbs=cls.percent_carbs,
+            percent_fats=cls.percent_fats)
         cls.session.add(account)
         cls.session.commit()
 
@@ -40,8 +61,7 @@ class Account:
             cls.BMR = 88.362 + (13.397 * cls.weight) + \
                 (4.799 * cls.height) - (5.677 * cls.age)
         if (cls.gender == 'F'):
-            cls.BMR = 447.593 + \
-                (9.247 * cls.weight) + \
+            cls.BMR = 447.593 + (9.247 * cls.weight) + \
                 (3.098 * cls.height) - (4.330 * cls.age)
 
         if (cls.activity_level == 1):
@@ -56,22 +76,26 @@ class Account:
             cls.calories = cls.BMR * 1.9
 
         return int(cls.calories)
-    @classmethod    
+
+    @classmethod
     def __crypt__(cls, password):
         cls.pw_bytes = password.encode('utf-8')
         return SHA256.new(cls.pw_bytes).hexdigest()
-    @classmethod    
 
+    @classmethod
     def check_password(cls, given_pass, password_hash):
         return SHA256.new(given_pass).hexdigest() == password_hash
 
-#TODO: Fix IvanlidRequestError
-    @classmethod   
-    def match_user_password(cls):
-        for row in cls.session.query(Account).all(): 
-            print(row.name)    
+    @classmethod
+    def match_user_password(cls, user, password):
+        for row in cls.session.query(models.Account).filter_by(name=user):
+            return row.password == cls.__crypt__(password)
 
+    @classmethod       
+    def update_field(cls, field, value):
+        pass
 
-Account(models.connect(), "Borisious", "952xcw74", 79, 183, 'M', 2, 21)
-#Account.add_account()
-#account.match_user_password() 
+Account(models.connect())
+Account.set_account("Felix", "6n9b3xcw74", 110, 194, 'M', 5, 28, 0.4, 0.4, 0.2)
+Account.add_account()
+print(Account.match_user_password("Borisious", "61b3xcw74"))
--- src/models.py
+++ src/models.py
@@ -1,28 +1,29 @@
-import sys 
+import sys
 import os
 
 from sqlalchemy import (
-    Column, 
-    ForeignKey, 
-    Integer, 
-    String, 
-    Float, 
-    Date, 
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Float,
+    Date,
     Enum,
     CheckConstraint,
     create_engine
 )
 from sqlalchemy.orm import (
-    relationship, 
-    sessionmaker, 
+    relationship,
+    sessionmaker,
     scoped_session,
     backref
 )
 from sqlalchemy.ext.declarative import declarative_base
- 
+
 
 MAX_LENGTH = 64
- 
+
+
 def connect():
     global ENGINE
     global Session
@@ -34,64 +35,89 @@ def connect():
 
 Base = declarative_base()
 engine = create_engine('sqlite:///cc.db', echo=False)
-session = scoped_session(sessionmaker(bind=engine, autocommit = False, autoflush = False)) 
+session = scoped_session(
+    sessionmaker(bind=engine, autocommit=False, autoflush=False))
 
-# Добавяне на ограничения
-# Еквивалнтност на users.id && accounts.id 
+# TODO: add constraints
+# TODO: check ForeignKeys (recomended stuff)
+
+
 class Account(Base):
     __tablename__ = 'accounts'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(MAX_LENGTH), nullable = False, unique = True)
-    password = Column(String, nullable = False)
-    weight = Column(Float, nullable = False)
-    height = Column(Integer, nullable = False)
-    gender = Column(Enum("M", "F", name = "genders_enum"),
-        nullable = False)   
-    activity_level = Column(Integer, 
-        CheckConstraint("activity_level > 0 AND activity_level <= 5"), 
-        nullable = False)
-    age = Column(Integer, nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(MAX_LENGTH), nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    weight = Column(Float, nullable=False)
+    height = Column(Integer, nullable=False)
+    gender = Column(Enum("M", "F", name="genders_enum"),
+                    nullable=False)
+    activity_level = Column(Integer,
+                            CheckConstraint(
+                                "activity_level > 0 AND activity_level <= 5"),
+                            nullable=False)
+    age = Column(Integer, nullable=False)
     recomended_calories = Column(Integer)
-    
+    recomended_proteins = Column(Float)
+    recomended_carbs = Column(Float)
+    recomended_fats = Column(Float)
+    percent_proteins = Column(Float,
+            CheckConstraint("percent_proteins > 0 AND percent_proteins < 1"))
+    percent_carbs = Column(Float,
+            CheckConstraint("percent_carbs > 0 AND percent_carbs < 1"))
+    percent_fats = Column(Float,
+            CheckConstraint("percent_fats > 0 AND percent_fats < 1"))
 
-    consumed_food = relationship("ConsumedFood", uselist=False, backref="accounts")
+    consumed_food = relationship(
+        "ConsumedFood", uselist=False, backref="accounts")
     stats = relationship("Statistics", uselist=False, backref="accounts")
 
 # TODO: Must add recomended and taken nutrition
+
+
 class Statistics(Base):
     __tablename__ = 'statistics'
-    id = Column(Integer, primary_key = True)
-    date = Column(Date, nullable = False)
-    recomended_calories = Column(Integer, nullable = False)
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, nullable=False)
+    recomended_calories = Column(Integer, nullable=False)
+    recomended_proteins = Column(Float, nullable=False)
+    recomended_carbs = Column(Float, nullable=False)
+    recomended_fats = Column(Float, nullable=False)
     consumed_calories = Column(Integer)
+    consumed_proteins = Column(Float)
+    consumed_carbs = Column(Float)
+    consumed_fats = Column(Float)
 
     account_id = Column(Integer, ForeignKey('accounts.id'))
 
+
 class Food(Base):
     __tablename__ = 'foodDB'
-    id = Column(Integer, primary_key = True)
-    name = Column(String(MAX_LENGTH), nullable = False, unique = True)
-    quantity = Column(Float, nullable = False)
-    calories = Column(Integer, nullable = False)
-    proteins = Column(Float, nullable = False)
-    carbs = Column(Float, nullable = False)
-    fats = Column(Float, nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(MAX_LENGTH), nullable=False, unique=True)
+    quantity = Column(Float, nullable=False)
+    calories = Column(Integer, nullable=False)
+    proteins = Column(Float, nullable=False)
+    carbs = Column(Float, nullable=False)
+    fats = Column(Float, nullable=False)
 
     # def __repr__(self):
     #     return "<Food(id='%s', name='%s', quantity='%s', calories='%s', proteins'%s', carbs = '%s', fats = '%s')>" % (
     #         self.id, self.name, self.quantity, self.calories, self.proteins, self.carbs,
     #         self.fats)
 
+# antoher fields for consumed food
+
+
 class ConsumedFood(Base):
-    __tablename__ = 'consumbedFood' 
-    id = Column(Integer, primary_key = True)
+    __tablename__ = 'consumbedFood'
+    id = Column(Integer, primary_key=True)
     multiplier_quantity = Column(Float)
-    name = Column(String(MAX_LENGTH), primary_key = False)
-    quantity = Column(Float, nullable = False)
-    calories = Column(Integer, nullable = False)
-    proteins = Column(Float, nullable = False)
-    carbs = Column(Float, nullable = False)
-    fats = Column(Float, nullable = False)
+    name = Column(String(MAX_LENGTH), primary_key=False)
+    quantity = Column(Float, nullable=False)
+    calories = Column(Integer, nullable=False)
+    proteins = Column(Float, nullable=False)
+    carbs = Column(Float, nullable=False)
+    fats = Column(Float, nullable=False)
 
     account_id = Column(Integer, ForeignKey('accounts.id'))
 
